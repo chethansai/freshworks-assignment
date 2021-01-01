ba877283528b8cba9b203149ec21bca80d707120
chethansaikrishna <chethansaikrishna@users.noreply.github.com>
2021-01-01 06:00:54 +0530

    create function to create key value ttl

--- crd.py
+++ crd.py
@@ -63,3 +63,64 @@ class crd:
             self.filelock.release()
             return False
 
+        # Creating and storing given Key, Value & optional Time-To-Live into file
+
+    def create(self, key='', value='', ttl=None):
+
+        # Checking if key is bounding to constraints
+        self.verify_key(key)
+
+        # Trigger for submission without entering key
+        if key == '':
+            raise Exception('No key was entered. Please enter the key.')
+
+        # Assigning value as none if no value is provided
+        if value == '':
+            value = None
+
+        # Checking if value size is less than 15kb
+        if sys.getsizeof(value) > 15000:
+            raise Exception("Size of the entered value exceeds capped 15KB size limit.")
+
+        if not self.check_file_size():
+            raise Exception('Size of the file exceeds 1 GB.')
+
+        self.datalock.acquire()
+        if key in self.data.keys():
+            self.datalock.release()
+
+            raise Exception('Key is already present.')
+
+        # ttl is Time-To-Live.
+        # It is the time upto which a key can be read or deleted
+        # The key expires after the set ttl
+        # ttl is given in form of seconds
+        # ttl is optional. In case of no input of ttl, the key value is considered to have no expiry
+        if ttl is not None:
+
+            ttl = int(time.time()) + abs(int(ttl))
+            tempdict = {'value': value, 'ttl': ttl}
+
+            self.data[key] = tempdict
+            self.filelock.acquire()
+            json.dump(self.data, fp=open(self.filepath, 'w'), indent=3)
+
+            self.filelock.release()
+            self.datalock.release()
+
+            return True
+
+
+        else:
+            ttl = None
+            tempdict = {'value': value, 'ttl': ttl}
+            self.data[key] = tempdict
+            self.filelock.acquire()
+            json.dump(self.data, fp=open(self.filepath, 'w'), indent=2)
+
+            self.filelock.release()
+            self.datalock.release()
+
+            return True
+
+
