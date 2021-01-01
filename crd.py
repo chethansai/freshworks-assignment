31c9889711dfa6f6a1537ebc6d5b65fdf9a5a083
chethansaikrishna <chethansaikrishna@users.noreply.github.com>
2021-01-01 05:55:47 +0530

    Created a crd class inside crd with file preapartion to store key-value pairs

--- crd.py
+++ crd.py
@@ -3,3 +3,34 @@ import os
 import sys
 import time
 import threading
+
+class crd:
+
+    def __init__(self):
+    #preparation of file and its access
+
+        #initiating file access variables
+        file_path = os.getcwd()
+        self.filepath = file_path + '/crd.json'
+        self.filelock = threading.Lock()
+        self.datalock = threading.Lock()
+
+
+        #acessing file data if file already created
+        try:
+            file = open(self.filepath, 'r')
+            filedata = json.load(file)
+            self.data = filedata
+            file.close()
+
+            if not self.check_file_size():
+                raise Exception('Size of the file storing data exceeded permitted 1GB')
+
+
+        #creating new file if file is not created previously
+        except:
+
+            file = open(self.filepath, 'w')
+            self.data = {}
+            self.ttldict = {}
+            file.close()
